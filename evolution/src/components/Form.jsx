07e1cf0effe2f4337f9eb5f2c69cc3c16933f3ff
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2022-01-03 16:03:50 +0530

    commit

--- evolution/src/components/Form.jsx
+++ evolution/src/components/Form.jsx
@@ -1,33 +1,74 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { styled } from "styled-components";
 export const Form = () => {
-  const [form, setForm] = useState({
-    title: "",
-  });
-  const handlechange = () => {};
+  const [form, setForm] = useState(null);
+  const [data, setData] = useState([]);
+  const ref = useRef(null);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetch("http://localhost:3001/receipes")
+      .then((d) => {
+        d.json();
+      })
+      .then((res) => {
+        setData(res);
+      });
+  };
+
+  const handleChange = (e) => {
+    console.log(ref.current.files);
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const addData = (e) => {
+    e.preventDefault();
+    const payload = form;
+    fetch("http://localhost:3001/receipes", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then(() => {
+      getData();
+      setForm(null);
+    });
+  };
   return (
     <div>
-      <form onSubmit={handleclick}>
+      <form onSubmit={addData}>
         <input
-          onChange={handlechange}
+          onChange={handleChange}
           name="title"
           type="text"
           placeholder="enter receipe"
         />
         <input
-          onChange={handlechange}
+          onChange={handleChange}
           name="ingredients"
           type="text"
           placeholder="enter ingredients"
         />
         <input
-          onChange={handlechange}
+          onChange={handleChange}
           name="timing"
           type="text"
           placeholder="time to cook"
         />
-        <input type="file" />
+        <input onChange={handleChange} ref={ref} name="image" type="file" />
         <input type="submit" value="Submit" />
       </form>
+
+      {/* {data.map((e) => (
+        <div>{e.title}</div>
+      ))} */}
+      <div>{data}</div>
     </div>
   );
 };
